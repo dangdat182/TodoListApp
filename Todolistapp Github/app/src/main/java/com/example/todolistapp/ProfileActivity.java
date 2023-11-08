54cc34786c773107962e7a45a91d224826fc98d5
Dexlo <danglequocdat182@gmail.com>
2023-11-08 23:01:11 +0700

    Xong Sign up with GG; Hien thi len profile.

--- Todolistapp Github/app/src/main/java/com/example/todolistapp/ProfileActivity.java
+++ Todolistapp Github/app/src/main/java/com/example/todolistapp/ProfileActivity.java
@@ -1,12 +1,73 @@
 package com.example.todolistapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.squareup.picasso.Picasso;
+
 public class ProfileActivity extends AppCompatActivity {
+    private GoogleSignInOptions gso;
+    private GoogleSignInClient gsc;
+    private Button buttonlogout;
+    private TextView profileEmail, profilename;
+    private ImageView profileImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // ActionBar actionBar = getSupportActionBar();
-        setContentView(R.layout.activity_home);
-}}
+        setContentView(R.layout.view_profile);
+
+        buttonlogout = findViewById(R.id.buttonlogout);
+        profileEmail = findViewById(R.id.profileEmail);
+        profilename = findViewById(R.id.profileName);
+        profileImage = findViewById(R.id.profileImage);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        gsc = GoogleSignIn.getClient(this,gso);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null){
+            String Name = account.getDisplayName();
+            String Email = account.getEmail();
+            Uri PhotoURL = account.getPhotoUrl();
+
+            Picasso.get().load(PhotoURL).into(profileImage);
+            profilename.setText(Name);
+            profileEmail.setText(Email);
+        }
+        buttonlogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logOut();
+            }
+        });
+    }
+
+    private void logOut() {
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        });
+
+    }
+}
