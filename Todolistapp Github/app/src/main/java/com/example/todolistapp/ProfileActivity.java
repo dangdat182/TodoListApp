5664050a571122062a3166393052d6a93d43bddb
UzumiKajitsu <minh75606@gmail.com>
2024-01-12 10:34:00 +0700

    sua cap nhat userprofile cho tk tu tao

--- Todolistapp Github/app/src/main/java/com/example/todolistapp/ProfileActivity.java
+++ Todolistapp Github/app/src/main/java/com/example/todolistapp/ProfileActivity.java
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -19,6 +22,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -29,6 +34,7 @@ public class ProfileActivity extends AppCompatActivity {
     private Button buttonlogout;
     private TextView profileEmail, profilename, doneCount, ongoingCount, outofdateCount;
     private ImageView profileImage;
+    private FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +46,8 @@ public class ProfileActivity extends AppCompatActivity {
         int done = extras.getInt("done");
         int ongoing = extras.getInt("ongoing");
         int outofdate = extras.getInt("outofdate");
+        String currentID = extras.getString("CurrentUID");
+        firestore = FirebaseFirestore.getInstance();
         buttonlogout = findViewById(R.id.buttonlogout);
         profileEmail = findViewById(R.id.profileEmail);
         profilename = findViewById(R.id.profileName);
@@ -50,6 +58,7 @@ public class ProfileActivity extends AppCompatActivity {
         doneCount.setText(String.valueOf(done));
         ongoingCount.setText(String.valueOf(ongoing));
         outofdateCount.setText(String.valueOf(outofdate));
+        Log.d("Test","Home Activity: "+ currentID);
         gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
@@ -65,6 +74,28 @@ public class ProfileActivity extends AppCompatActivity {
             Picasso.get().load(PhotoURL).into(profileImage);
             profilename.setText(Name);
             profileEmail.setText(Email);
+        }
+        else{
+            View emailview = findViewById(R.id.emailLayout);
+            emailview.setVisibility(View.GONE);
+            firestore.collection("UserID")
+                    .whereEqualTo("UserID", currentID)
+                    .get()
+                    .addOnSuccessListener(queryDocumentSnapshots -> {
+                                if (!queryDocumentSnapshots.isEmpty()) {
+                        DocumentSnapshot documentSnapshot = queryDocumentSnapshots.getDocuments().get(0);
+
+
+                        String name = documentSnapshot.getString("Fullname");
+                        profilename.setText(name);
+                    }
+
+                    })
+                    .addOnFailureListener(e -> {
+
+                        Toast.makeText(ProfileActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
+
         }
         buttonlogout.setOnClickListener(new View.OnClickListener() {
             @Override
